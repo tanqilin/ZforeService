6e7d912c315fd816d03d4ca9af8ba06d0145471a
taniqlin <1135574399@qq.com>
2017-11-25 17:42:02 +0800

    创建自定义安装类库

---
+++ Installer/InstallSetup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Installer
+{
+    /// <summary>
+    /// 自定义安装
+    /// </summary>
+    public class InstallSetup
+    {
+
+    }
+}
